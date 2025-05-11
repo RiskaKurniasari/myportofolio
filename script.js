57c995113bc7dd63efd27a1d0bade56836c9d3ed
RiskaKurniasari <rkurniasari51@gmail.com>
2025-05-11 16:28:32 +0700

    myportofolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Placeholder: add typing effect if desired
+const typed = new Typed('.typed-text', {
+    strings: ["Developer"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+  });
+  
